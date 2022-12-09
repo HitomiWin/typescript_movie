5dbadc4d0d0b5dca782ba7f7d885dc1d43c46a18
HitomiWin <hitomi.winberg@gmail.com>
2022-12-09 21:56:56 +0100

    add hero contaner and seach field

---
+++ src/pages/components/Hero.tsx
@@ -0,0 +1,28 @@
+import styles from "../../css/Hero.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <div className={`${styles.heroContainer} wContainer`}>
+        <h1>Movies changes your life</h1>
+        <p>Millions of movies, TV shows and people to discover. Explore now.</p>
+        <div className={styles.searchContainer}>
+          <form className={styles.form}>
+            <input
+              type="search"
+              placeholder="Search for a movie, person..."
+              className={styles.searchInput}
+            />
+            <button type="submit" className={styles.searchBtn}>
+              <FontAwesomeIcon className={styles.fas} icon={faSearch} />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
